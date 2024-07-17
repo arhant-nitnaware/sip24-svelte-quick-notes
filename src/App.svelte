48bb292a28c50e33766d683d33c8e93b8b68ace6
Arhant Nitnaware <arhant00@gmail.com>
2024-07-17 15:50:17 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,73 @@
 <script>
- 
+    import { onMount } from "svelte";
+
+  let title = '';
+  let text = '';
+  let pages = [];
+  let currentPageIndex = 0;
+
+  function addPage(){
+    pages.push("new page")
+    selectPage(pages.length ? pages.length - 1 : 0)
+  }
+
+  function saveNote(){
+    const storePageName = pages[currentPageIndex]
+    if(storePageName != title){
+      localStorage.removeItem(storePageName)
+      pages[currentPageIndex] = title
+    }
+    pages[currentPageIndex] = title;
+    localStorage.setItem(title, text)
+    localStorage.setItem('pages', JSON.stringify(pages))
+  }
+
+  function selectPage(index){
+    currentPageIndex = index
+    title = pages[currentPageIndex]
+    text = localStorage.getItem(title)
+  }
+
+  onMount(() => {
+    const savedPages = localStorage.getItem('pages')
+    
+    if (savedPages){
+      pages = JSON.parse(savedPages);
+      title = pages[currentPageIndex] || "new page"
+      text = localStorage.getItem(title)
+    }
+    else{
+      addPage();
+    }
+  })
+
 </script>
 
-<main>
-  Hello World
+<aside class="fixed top-0 left-0 w-60 h-screen z-40">
+  <div class="border-r overflow-y-auto py-5 px-3 h-full">
+    <ul class="space-y-5">
+      {#each pages as page, index}
+      <li>
+        <button on:click={() => selectPage(index)} class="{index == currentPageIndex ? "bg-gray-200" : ""} rounded px-4 py-1.5 border-black">{page || "new page"}</button>
+      </li>
+      {/each}
+      <li class="text-center">
+        <button on:click={addPage}>+ add page</button>
+      </li>
+      
+    </ul>
+  </div>
+
+</aside>
+
+<main class="p-4 ml-60 h-auto">
+  <div class="grid grid-cols-2 items-center mb-3">
+    <h1 class="text-2xl font-bold" contenteditable bind:textContent={title}>{title || "new page"}</h1>
+    <button class="rounded text-white bg-gray-800 px-10 py-1 border border-black ml-auto hover:bg-gray-900" on:click={saveNote}>save</button>
+  </div>
+  <hr/>
+  <!-- <input class="block w-full border rounded-lg p-2.5 mt-2" bind:value={title} type="Text"> -->
+  <textarea class="block w-full mt-3 h-96 rounded-lg p-5" bind:value={text}></textarea>
 </main>
 
 <style>
